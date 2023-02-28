0fb5a80e0c524af482fc7ae564ab0e53ac65b09d
Boris Stojchevski <stojcevskib.02@gmail.com>
2023-02-28 19:48:37 +0100

    Mobile Compatability

--- Project_IT/Scripts/modernizr-2.8.3.js
+++ Project_IT/Scripts/modernizr-2.8.3.js
@@ -549,7 +549,6 @@ window.Modernizr = (function( window, document, undefined ) {
         //  and then querying the style.background property value for the number of
         //  occurrences of "url(" is a reliable method for detecting ACTUAL support for this!
 
-        setCss('background:url(https://),url(https://),red url(https://)');
 
         // If the UA supports multiple backgrounds, there should be three occurrences
         //   of the string "url(" in the return value for elemStyle.background
