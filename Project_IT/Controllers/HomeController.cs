4f4dfd86cfdbb064610bd8f0109d5de65dd1bd44
Boris Stojchevski <stojcevskib02@gmail.com>
2024-01-23 13:27:29 +0100

    Add log4net

--- Project_IT/Controllers/HomeController.cs
+++ Project_IT/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,9 @@ namespace Project_IT.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private static log4net.ILog Log { get; set; }
+        ILog log = log4net.LogManager.GetLogger(typeof(ReservationsController));
+
         public ActionResult Index()
         {
             return View();
--- Project_IT/Global.asax.cs
+++ Project_IT/Global.asax.cs
@@ -1,3 +1,5 @@
+using log4net;
+using Project_IT.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,27 @@ namespace Project_IT
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        private static log4net.ILog Log { get; set; }
+        ILog log = log4net.LogManager.GetLogger(typeof(ReservationsController));
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            log4net.Config.XmlConfigurator.Configure();
+            
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            if (ex == null) return;
+
+            log.Error(ex.Message);
+
         }
     }
 }
