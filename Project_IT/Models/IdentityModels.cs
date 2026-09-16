42140acc61aa7c0f6027862e50a84e7323b2fc62
Boris Stojchevski <stojcevskib.02@gmail.com>
2026-09-16 21:21:31 +0200

    git deploy test

--- Project_IT/Models/IdentityModels.cs
+++ Project_IT/Models/IdentityModels.cs
@@ -24,7 +24,7 @@ namespace Project_IT.Models
         public DbSet<FeedItem> FeedItems { get; set; }
 
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnectionStage", throwIfV1Schema: false)
         {
         }
 
