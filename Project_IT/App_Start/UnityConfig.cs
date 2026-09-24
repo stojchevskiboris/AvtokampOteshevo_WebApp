5cb58dbbabfc3dd8122115d6151d19338e7b70bb
Boris Stojchevski <stojcevskib.02@gmail.com>
2026-09-24 23:07:15 +0200

    Unity container working version

--- Project_IT/App_Start/UnityConfig.cs
+++ Project_IT/App_Start/UnityConfig.cs
@@ -1,10 +1,12 @@
+using System.Web.Mvc;
+using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+using Unity.Mvc5;
+using Project_IT.Controllers;
 using Project_IT.Models;
 using Project_IT.Services.Implementations;
 using Project_IT.Services.Interfaces;
-using System.Web.Mvc;
-using Unity;
-using Unity.AspNet.Mvc;
-using Unity.Lifetime;
 
 namespace Project_IT
 {
@@ -14,17 +16,20 @@ namespace Project_IT
         {
             var container = new UnityContainer();
 
-            // 1. Register DbContext (Per HTTP Request)
+            // 1. Force Unity to use the parameterless constructor for AccountController
+            container.RegisterType<AccountController>(new InjectionConstructor());
+
+            // 2. Register DbContext (Per HTTP Request)
             container.RegisterType<ApplicationDbContext>(new HierarchicalLifetimeManager());
 
-            // 2. Register Services
+            // 3. Register Application Services
             container.RegisterType<ISitemapService, SitemapService>(new HierarchicalLifetimeManager());
             container.RegisterType<IFeedItemService, FeedItemService>(new HierarchicalLifetimeManager());
             container.RegisterType<IReservationService, ReservationService>(new HierarchicalLifetimeManager());
             container.RegisterType<INewsService, NewsService>(new HierarchicalLifetimeManager());
             container.RegisterType<IFileManagerService, FileManagerService>(new HierarchicalLifetimeManager());
 
-            // 3. Set Unity as the ASP.NET MVC Dependency Resolver
+            // 4. Set Unity as the ASP.NET MVC Dependency Resolver
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
