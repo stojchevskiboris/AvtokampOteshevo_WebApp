ab847246502c808f60a1f30ec05d53af6c3fcf4c
Boris Stojchevski <boriss@nextsense.com>
2026-09-24 17:02:47 +0200

    UnitiContainer fixes

--- Project_IT/App_Start/UnityConfig.cs
+++ Project_IT/App_Start/UnityConfig.cs
@@ -1,10 +1,10 @@
-using System.Web.Mvc;
-using Unity;
-using Unity.Lifetime;
-using Unity.Mvc5;
 using Project_IT.Models;
 using Project_IT.Services.Implementations;
 using Project_IT.Services.Interfaces;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+using Unity.Lifetime;
 
 namespace Project_IT
 {
@@ -14,16 +14,17 @@ namespace Project_IT
         {
             var container = new UnityContainer();
 
-            // Register ApplicationDbContext as HierarchicalLifetimeManager (PerRequest scoped)
+            // 1. Register DbContext (Per HTTP Request)
             container.RegisterType<ApplicationDbContext>(new HierarchicalLifetimeManager());
 
-            // Register Services
+            // 2. Register Services
             container.RegisterType<ISitemapService, SitemapService>(new HierarchicalLifetimeManager());
             container.RegisterType<IFeedItemService, FeedItemService>(new HierarchicalLifetimeManager());
             container.RegisterType<IReservationService, ReservationService>(new HierarchicalLifetimeManager());
             container.RegisterType<INewsService, NewsService>(new HierarchicalLifetimeManager());
             container.RegisterType<IFileManagerService, FileManagerService>(new HierarchicalLifetimeManager());
 
+            // 3. Set Unity as the ASP.NET MVC Dependency Resolver
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
